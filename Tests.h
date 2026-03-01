63c59f2d2b14b1ba31bc34a77f871710a49fdc46
StArbLEE <axel20378@gmail.com>
2026-03-02 00:30:45 +0300

    Tests.h

---
+++ Tests.h
@@ -0,0 +1,8 @@
+#pragma once
+bool TESTGetPatientsByAstma();
+bool TESTGetPatientsByRak();
+bool TESTGetPatientsByArtrit();
+bool TESTGetPatientsByMedCard100000_150000();
+bool TESTGetPatientsByMedCard200000_400000();
+bool RunAllTests();
+bool EXPECT_EQ(int expected, int actual);
